9da8180bfc9017a5bb33615ce43e607868991906
Omari <gaskins.30@osu.edu>
2018-06-27 23:58:37 -0400

    added delete button, isn't working yet

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-console.log('It works!');
-const button = document.querySelector('button')
-const updateText = function() {
-  const p = document.querySelector('h1')
-  p.textContent = "ChrisGiving: The Return of Chrismess"
-  
-}
-button.addEventListener('click', updateText)
 
-const button2 = document.querySelector('#formStuff')
+const form = document.querySelector('#formStuff')
+
+
 
 const addToListFilm = function(ev){
   ev.preventDefault()
+  const flickArray = []
   const fName = ev.target
   const movieName = fName.movieName.value
-  const fitem  = document.createElement('li')
-  fitem.textContent = movieName
-  const list = document.querySelector('#listStart')
-  list.appendChild(fitem)
-  fName.reset()
-}
+  const fitem  = document.createElement('span')
 
-button2.addEventListener('submit',addToListFilm)
+  fitem.textContent = movieName
+
+  const list = document.querySelector('#listStart')
+
+  list.appendChild(fitem)
+  const testingAdd = flickArray.push(movieName)
+  
+  console.log(flickArray)
+  fName.reset()
+  addToListYear(ev)
+}
+const deleteButton = function(ev){
+    yes.OnClick() = function(){
+      testingAdd.pop(movieName)
+    }
+  }
+form.addEventListener('deletingButton', deleteButton)
+form.addEventListener('submit',addToListFilm)
 
 const addToListYear = function(event){
-  event.preventDefault()
+  console.log("add list to function")
   const yTime = event.target
+  console.log(yTime.yearTime.value)
   const yearTime = yTime.yearTime.value
-  const yItem = document.createElement('li')
+
+  const yItem = document.createElement('span')
+  console.log(yearTime)
   yItem.textContent = yearTime
   const listing = document.querySelector('#yearInput')
   listing.appendChild(yItem)
-  yTime.reset()
 }
-button2.addEventListener('submit',addToListYear)
